bde41b9ccd76a0d24e75a7c26fb6706c70decd72
ayuputrii <ayuarmadani.pm@gmail.com>
2021-03-24 20:19:39 +0700

    add splash screen

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, {useEffect} from 'react';
 import RouterNavigation from './Apps/config/router';
 import {Provider} from 'react-redux';
 import {Provider as PaperProvider} from 'react-native-paper';
 import {store} from './Apps/config/redux/store';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  });
   return (
     <Provider store={store}>
       <PaperProvider>
